8a87026f305a1f4f880d51564d34e0b1802809cd
joel790 <eyuelkassahun3@gmail.com>
2023-03-10 13:49:39 +0300

    chatGpt-v1

---
+++ lib/constants/api_consts.dart
@@ -0,0 +1,2 @@
+String BASE_URL = "https://api.openai.com/v1";
+String API_KEY = "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";
---
+++ lib/constants/constants.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+Color scaffoldBackgroundColor = Color(0xFF32033B);
+Color cardColor = const Color(0xFF444654);
